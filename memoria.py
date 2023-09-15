2439150acb9f381ef1e3d7180ef9203815d73e1f
ETERNALCODER0 <netoarriola261@gmail.com>
2023-09-14 19:38:44 -0600

    Version 4 Probado y validado con flake8

--- memoria.py
+++ memoria.py
@@ -1,5 +1,9 @@
-from random import *
-from turtle import *
+from random import shuffle
+from turtle import (
+    up, goto, down, color, begin_fill, forward, left,
+    end_fill, write, clear, shape, stamp, update, ontimer,
+    setup, addshape, hideturtle, tracer, onscreenclick, done
+)
 from freegames import path
 
 car = path('car.gif')
@@ -8,6 +12,7 @@ state = {'mark': None}
 hide = [True] * 64
 # Global variable to keep track of the number of "taps".
 tap_count = 0
+
 
 def square(x, y):
     "Draw white square with black outline at (x, y)."
@@ -21,13 +26,16 @@ def square(x, y):
         left(90)
     end_fill()
 
+
 def index(x, y):
     "Convert (x, y) coordinates to tiles index."
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
+
 def xy(count):
     "Convert tiles count to (x, y) coordinates."
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+
 
 def tap(x, y):
     "Update mark and hidden tiles based on tap."
@@ -43,12 +51,14 @@ def tap(x, y):
         hide[mark] = False
         state['mark'] = None
 
+
 def draw_tap_count():
     "Draw the tap count on the screen."
     up()
     goto(-180, 180)  # Position to display the number
     color('black')
     write(f'Taps: {tap_count}', font=('Arial', 16, 'normal'))
+
 
 def draw():
     "Draw image and tiles."
@@ -70,7 +80,7 @@ def draw():
         goto(x + 2, y)
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
-    
+
     draw_tap_count()  # Call the function to display the number of taps.
     update()
 
@@ -80,8 +90,8 @@ def draw():
         color('black')
         write('¡Has ganado!', font=('Arial', 24, 'bold'))
 
-
     ontimer(draw, 100)
+
 
 def all_tiles_uncovered():
     "Check if all tiles have been uncovered."
